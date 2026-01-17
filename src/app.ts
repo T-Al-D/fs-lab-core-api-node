4dc75b3a3e74774187c97148a898ab6c25c4b52c
T-Al-D <t.private@gmx.net>
2026-01-17 18:15:07 +0100

    added '/' get-request

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,13 @@ import express, { Request, Response } from "express";
 
 export const app = express();
 
+app.get("/", (_req, res) => {
+  res.json({
+    name: "fs-lab-core-api-node",
+    status: "running",
+  });
+});
+
 app.get("/health", (_req: Request, res: Response) => {
   res.json({ status: "ok" });
 });
